1080cdb4ce3c2ef97e85a1785b1e0514e756718c
Barbara Lima <Babiplima123>
2023-06-19 15:44:27 -0300

    first commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Barbara Lima on 29/05/23.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
